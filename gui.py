4188dbc14d2ebbb3c3ce8116a549b6859515db12
Deep Baro <137148197+deepbaro@users.noreply.github.com>
2023-07-12 12:29:14 +0530

    Update gui.py
    opendiag box added, few other improvement

--- gui.py
+++ gui.py
@@ -1,11 +1,18 @@
 import customtkinter as ctk
 from tkinter import filedialog
-
+from PIL import Image, ImageTk
 
 #buttons
 def add_todo():
-    img_name = filedialog.askopenfilename()
-    print(img_name)
+    img_path = filedialog.askopenfilename()
+    image_toview =  ctk.CTkImage(Image.open(img_path), size=(250, 250)) 
+    print(img_path)
+
+    view_box.configure(image=image_toview)
+
+
+def button_event():
+     pass
 #setting up tkinter
 ctk.set_appearance_mode("Dark")
 root = ctk.CTk()
@@ -19,10 +26,19 @@ title = ctk.CTkLabel(root, text="Facial Emotion Detector", font=comic_font)
 title.pack(padx=10, pady=(40, 20))
 
 
-scrollable_frame = ctk.CTkFrame(root, width=500, height=200)
-scrollable_frame.pack()
 
-add_button = ctk.CTkButton(root, text="Upload Your Image", width=500, command=add_todo)
+frame_a = ctk.CTkFrame(root)
+frame_a.pack()
+
+frame_b = ctk.CTkFrame(root)
+frame_b.pack()
+
+view_box = ctk.CTkLabel(master=frame_a, text="")
+view_box.pack()
+add_button = ctk.CTkButton(frame_b, text="Upload Your Image", width=500, command=add_todo)
 add_button.pack(pady=20)
+
+
+
 
 root.mainloop()
